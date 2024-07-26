0f50bffacf50d12b925b2aceb11ce5af99e6b478
parlough <parlough@users.noreply.github.com>
2024-07-26 13:35:25 +0000

    deploy: 8f61fafa03c522f7d70019cbb688574a22443fe8

--- web/simplistic_editor/flutter_bootstrap.js
+++ web/simplistic_editor/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "882095881"
+    serviceWorkerVersion: "1218979670"
   }
 });
--- web/web_dashboard/flutter_bootstrap.js
+++ web/web_dashboard/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2787853730"
+    serviceWorkerVersion: "4050316680"
   }
 });
