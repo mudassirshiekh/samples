f4b88dbb2a1f6d50c831b3f5fa3d811a2dd61dbb
ericwindmill <ericwindmill@users.noreply.github.com>
2024-07-29 14:46:21 +0000

    deploy: c75d9fa983edc4c7dad9b282c47cf3cbf75425f8

--- web/game_template/flutter_bootstrap.js
+++ web/game_template/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3277399103"
+    serviceWorkerVersion: "3828666481"
   }
 });
