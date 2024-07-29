e7077791d2ccd07f1cb13cc177bfb9722b078772
domesticmouse <domesticmouse@users.noreply.github.com>
2024-07-29 09:04:07 +0000

    deploy: e566321bc2e48e1727f3f166cadf81385a91b3bd

--- web/provider_shopper/flutter_bootstrap.js
+++ web/provider_shopper/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3953749648"
+    serviceWorkerVersion: "2634364869"
   }
 });
