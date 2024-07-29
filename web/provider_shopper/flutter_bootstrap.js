87988535a9705a7e31f00a96a1cb264f0bae9792
reidbaker <reidbaker@users.noreply.github.com>
2024-07-29 15:28:31 +0000

    deploy: f443fa60360d1e3a28c44626f4c146299259cc9b

--- web/provider_shopper/flutter_bootstrap.js
+++ web/provider_shopper/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3663985811"
+    serviceWorkerVersion: "2702717356"
   }
 });
