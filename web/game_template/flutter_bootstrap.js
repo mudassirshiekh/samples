431dd2dcdcc08404953dc9b6b85b95a036b6b063
domesticmouse <domesticmouse@users.noreply.github.com>
2024-07-29 09:09:37 +0000

    deploy: b6065c409e00c7badb9e370c1ab894c197c90949

--- web/game_template/flutter_bootstrap.js
+++ web/game_template/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "733173097"
+    serviceWorkerVersion: "3277399103"
   }
 });
