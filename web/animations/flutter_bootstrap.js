d5fa0cca5eee36e600a5926df2e463055f2b3506
domesticmouse <domesticmouse@users.noreply.github.com>
2024-08-07 07:56:38 +0000

    deploy: 6b6104c9c99dac534ee8fe74ceb893f4da7ffc59

--- web/animations/flutter_bootstrap.js
+++ web/animations/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "355215188"
+    serviceWorkerVersion: "3920420439"
   }
 });
--- web/simplistic_editor/flutter_bootstrap.js
+++ web/simplistic_editor/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "558393447"
+    serviceWorkerVersion: "1347320698"
   }
 });
