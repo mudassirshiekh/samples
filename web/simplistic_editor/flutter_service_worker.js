e1fd100852c37244bc79de0fa1ebdef7cc5529f2
parlough <parlough@users.noreply.github.com>
2024-08-26 09:33:15 +0000

    deploy: cd5956dd5d95c5d5cd84cae4234a053394b8e70f

--- web/simplistic_editor/flutter_service_worker.js
+++ web/simplistic_editor/flutter_service_worker.js
@@ -20,7 +20,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "main.dart.js": "0037f02a09c06ca38b7b053dc7a4292e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "5f28b101a23cda222084598333ffdc2e",
-"flutter_bootstrap.js": "928039b50bc627f3a31d6ef6e6e8b622",
+"flutter_bootstrap.js": "e24869b5599d5a6db9608cb61bf3372b",
 "canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
