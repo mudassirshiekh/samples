27db4b6905a3efbf497268bfb69d30e76f3c725b
domesticmouse <domesticmouse@users.noreply.github.com>
2024-08-29 01:07:23 +0000

    deploy: f4e92d710d52f2b0f10557dd4183c273a0516470

--- web/navigation_and_routing/flutter_service_worker.js
+++ web/navigation_and_routing/flutter_service_worker.js
@@ -20,7 +20,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "main.dart.js": "1215833f87cd54603aadcadefdd9c894",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "9c336dc978923faadf6aa0ad75d0ea12",
-"flutter_bootstrap.js": "30f7da745952d2e42ecfb51b75b75e63",
+"flutter_bootstrap.js": "5f0b4ec904781a861adc1c59e8ef1077",
 "canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
--- web/simplistic_editor/flutter_service_worker.js
+++ web/simplistic_editor/flutter_service_worker.js
@@ -20,7 +20,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "main.dart.js": "0037f02a09c06ca38b7b053dc7a4292e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "5f28b101a23cda222084598333ffdc2e",
-"flutter_bootstrap.js": "fff6cdd3884b6f59e5a8a81dbcddc2c8",
+"flutter_bootstrap.js": "7814bb1035d2fdc3a07a7a99f849cd5c",
 "canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
