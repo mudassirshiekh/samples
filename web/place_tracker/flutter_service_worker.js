e1b965ea1183beeb46137eb19198122accb1acf2
domesticmouse <domesticmouse@users.noreply.github.com>
2024-08-31 02:25:19 +0000

    deploy: 82a8a8411ef473af5964ae5751407fea6678ac34

--- web/place_tracker/flutter_service_worker.js
+++ web/place_tracker/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "main.dart.js": "6aad1e496a4d7f3e9176920693d85c71",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "d8b1411a8d58c36c6abe2f9380d1c88d",
-"flutter_bootstrap.js": "8784b4b6f4bc72dec483dc2288815f2f",
+"flutter_bootstrap.js": "b4b7674fe87f2832596cfd74ebd16a92",
 "canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
