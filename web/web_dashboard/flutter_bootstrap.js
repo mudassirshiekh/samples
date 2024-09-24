f7301ded1601828489fee9d0c8b976c96a068796
domesticmouse <domesticmouse@users.noreply.github.com>
2024-09-24 23:46:33 +0000

    deploy: 259403e8a6e7201c5630c4725ebb9805cdb05687

--- web/web_dashboard/flutter_bootstrap.js
+++ web/web_dashboard/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "548715727"
+    serviceWorkerVersion: "531375667"
   }
 });
