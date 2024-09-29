b45dee31fb6f4ea13b42546b32b82b00ebcd0cfe
domesticmouse <domesticmouse@users.noreply.github.com>
2024-09-29 05:12:29 +0000

    deploy: 51a386e1c8e45c79a1550019109073c587dffcf4

--- web/web_dashboard/flutter_service_worker.js
+++ web/web_dashboard/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b
 "firebase_init.js": "622b108cf1fb4d6527c998381e317752",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"flutter_bootstrap.js": "d0df16cec2ec6d5861099c4538a2b475",
+"flutter_bootstrap.js": "5c37d9f4a0ae464d5afcd2f54f4dac3b",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "index.html": "18aaf2e497fd84025c8e0d56b518d072",
 "/": "18aaf2e497fd84025c8e0d56b518d072",
